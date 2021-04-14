daba7c46abbd25ee7ca04e505cdd83b97724429d
Jorge Lucas <jorgelucas@pixelexperience.org>
2021-04-14 00:50:27 -0300

    raphael: Add missing TARGET_BOOT_ANIMATION_RES
    Change-Id: I3879ff51a5484097ca073997599e754363eaa6ef

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -18,6 +18,7 @@ $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common PixelExperience stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080p
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_raphael
